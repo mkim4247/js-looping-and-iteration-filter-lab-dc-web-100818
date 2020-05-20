5302e0ad9d1c88a3453d478b2abdbcbba4fb3006
Michael Kim <m.kim4247@gmail.com>
2020-05-20 16:23:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 // Code your solution in this file
-function findMatching(arr, str) {
-  return arr.filter(function(driver) { return driver.toLowerCase() === str.toLowerCase(); });
+function findMatching(collection, names) {
+  return collection.filter(name => name.toLowerCase() === names.toLowerCase()
+  )
 }
 
-function fuzzyMatch(arr, str) {
-  return arr.filter(function(driver) {
-    return driver.slice(0, str.length) === str;
-  });
+function fuzzyMatch(collection, namesBeginWith) {
+  return collection.filter(function(name) {
+    for (const i of name) {
+      if (name.charAt(i) === namesBeginWith.charAt(i))
+      return true;
+    }
+  })
 }
 
-function matchName(arr, str) {
-  return arr.filter(function(driver) {
-    return driver.name === str
-  });
+function matchName(collection, names) {
+  return collection.filter(function deepIterator(name) {
+    if (typeof name === 'object') {
+      for (const key in name) {
+        deepIterator(name[key]);
+        if (name[key] === names) {
+          return true;
+        }
+      }
+    }
+  })
 }
